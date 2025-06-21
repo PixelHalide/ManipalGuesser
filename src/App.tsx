6ff27932fe2a21cf1195b7028dc4e82be70cf13b
PixelHalide <taintedxenon@gmail.com>
2025-06-21 16:34:17 +0400

    Init

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./Navbar"
+import Footer from "./Footer";
+import Home from "./Home";
+
+function App() {
+  const [dark, set_dark] = useState(true);
+
+  const switchTheme = () => {
+    set_dark(!dark);
+  }
+
+  return (
+      <div className={`min-h-screen flex flex-col bg-[#EEEEEE] dark:bg-[#181818] transition-all ${dark ? 'dark' : ''}`}>
+        <Router>
+          <Navbar
+          switchTheme={switchTheme}
+          darkState={dark}
+          />
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </Router>
+      </div>
+  )
+}
+
+export default App
---
+++ src/Navbar.tsx
@@ -0,0 +1,47 @@
+import dark_logo from './assets/logo.png'
+import logo from './assets/logo_black.png'
+
+interface DarkModeProp {
+    darkState: boolean,
+    switchTheme: () => void
+}
+
+const Navbar = ({switchTheme, darkState} : DarkModeProp) => {
+
+  return (
+    <div className='sticky top-0 z-30 bg-[#EEEEEE] dark:bg-[#212121] dark:text-neutral-200 border-[#383838] dark:border-gray-300 border-b-2'>
+        <nav className="flex flex-wrap flex-col md:flex-row justify-between items-center mb-2 pt-2 px-4 text-sm md:text-lg">
+            <div>
+              <div className='flex flex-row'>
+                <img src={`${darkState ? dark_logo : logo}`} alt="Logo" className='w-10 h-10'/>
+                <p className='font-prompt font-extrabold text-2xl md:text-4xl mx-4'>ManipalGuessr</p>
+              </div>
+            </div>
+              <div className='flex flex-col md:flex-row justify-center items-center'>
+                <div className='hover:bg-gray-200 p-2 rounded-2xl transition duration-200 dark:hover:text-black'>
+                  <div className="py-1 px-4 rounded transition-all">
+                    Home
+                  </div>
+                </div>
+                <div className='hover:bg-gray-200 p-2 rounded-2xl transition duration-200 dark:hover:text-black'>
+                  <div className="py-1 px-4 rounded hover:text-gray-700 transition-all">
+                    About Us
+                  </div>
+                </div>
+                <div className='hover:bg-gray-200 p-2 rounded-2xl transition duration-200 dark:hover:text-black'>
+                  <div className="py-1 px-4 rounded hover:text-gray-700 transition-all">
+                    Contact Us
+                  </div>
+                </div>
+              </div>
+            <div className='hover:bg-neutral-900 px-2 py-1.5 rounded-2xl transition duration-200 bg-black text-white dark:hover:text-neutral-200 dark:text-black dark:bg-white cursor-pointer'>
+              <div onClick={() => {switchTheme()}} className="px-4 rounded transition-all active:scale-110">
+                Switch Theme
+              </div>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
