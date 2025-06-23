5c95e65715679a6d30758c66a535df9b3f126762
PixelHalide <taintedxenon@gmail.com>
2025-06-24 00:50:25 +0400

    Progress bar

---
+++ src/ProgressBar.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+interface ProgressBarProps {
+  targetProgress: number;
+  intervalTime: number;
+}
+
+const ProgressBar = ({ targetProgress, intervalTime}: ProgressBarProps) => {
+  const [currentProgress, setCurrentProgress] = useState(0);
+
+  useEffect(() => {
+    if (currentProgress >= targetProgress) {
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setCurrentProgress((prevProgress) => {
+        const nextProgress = prevProgress + 1;
+        if (nextProgress >= targetProgress) {
+          clearInterval(interval);
+          return targetProgress;
+        }
+        return nextProgress;
+      });
+    }, intervalTime);
+
+    return () => clearInterval(interval);
+  }, [currentProgress, targetProgress, intervalTime]);
+
+  return (
+    <div className="w-full bg-gray-200 rounded-full h-8 relative overflow-hidden mt-4 mb-2">
+      <div
+        className="bg-blue-500 h-8 rounded-full"
+        style={{ width: `${currentProgress.toFixed(0)}%` }}
+      ></div>
+      <span className="absolute inset-0 flex items-center justify-center text-xs font-medium text-gray-800">
+        {Math.round(currentProgress)}%
+      </span>
+    </div>
+  );
+};
+
+export default ProgressBar;
--- src/ScoreScreen.tsx
+++ src/ScoreScreen.tsx
@@ -1,4 +1,5 @@
 import ResultMap from "./ResultMap"
+import ProgressBar from "./ProgressBar"
 
 interface ScoreProp {
     attainedScore: number,
@@ -8,15 +9,18 @@ interface ScoreProp {
 
 const ScoreScreen = ({ attainedScore, clickedLocation, actualLocation } : ScoreProp) => {
   return (
-    <div className='fixed inset-0 bg-black/50 z-50'>
+    <div className='fixed inset-0 bg-black/50 z-50 flex justify-center items-center'>
         <div className='flex flex-col justify-center p-10 bg-white rounded-lg text-center'>
             <ResultMap
             clickedLocation={clickedLocation}
             actualLocation={actualLocation}
             />
+            <ProgressBar
+            targetProgress={((attainedScore*100)/5000)}
+            intervalTime={20}
+            />
             <p>Attained Score: {attainedScore.toFixed(0)}</p>
         </div>
-
     </div>
   )
 }
