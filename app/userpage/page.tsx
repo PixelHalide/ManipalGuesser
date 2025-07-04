80e5591dfbcffe4366875623c12611f4b05593d2
PixelHalide <taintedxenon@gmail.com>
2025-07-04 11:53:45 +0400

    img to Image

--- app/userpage/page.tsx
+++ app/userpage/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from '@/auth'
+import Image from 'next/image';
 import { logout } from '@/lib/actions/auth';
 
 const UserPage = async () => {
@@ -15,12 +16,12 @@ const UserPage = async () => {
     <div className='flex flex-col items-center'>
         <h1 className="text-black dark:text-neutral-200 text-6xl text-center mt-14">Welcome, {session.user?.name}</h1>
         <div className="p-6">
-            <img
+            <Image
             src={session.user.image ?? "/default-user-icon.png"}
             alt="User Icon"
             className="w-30 h-30 rounded-full object-cover"
             >
-            </img>
+            </Image>
         </div>
         <button onClick={logout} className="inline-flex h-12 items-center justify-center rounded-md bg-red-500 px-6 font-medium text-neutral-50 transition active:scale-110 cursor-pointer">Sign Out</button>
     </div>
